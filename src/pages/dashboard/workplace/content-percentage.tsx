9f456938a5a231325e9044b3a22bced3d0cff259
liyu <liyu@espressif.com>
2022-01-07 14:40:11 +0800

    初始化

--- src/pages/dashboard/workplace/content-percentage.tsx
+++ src/pages/dashboard/workplace/content-percentage.tsx
@@ -31,54 +31,54 @@ function PopularContent() {
       headerStyle={{ borderBottom: 0 }}
     >
       <Spin loading={loading} style={{ display: 'block' }}>
-        <DonutChart
-          autoFit
-          height={340}
-          data={data}
-          radius={0.7}
-          innerRadius={0.65}
-          angleField="count"
-          colorField="type"
-          color={['#21CCFF', '#313CA9', '#249EFF']}
-          interactions={[
-            {
-              type: 'element-single-selected',
-            },
-          ]}
-          tooltip={{ showMarkers: false }}
-          label={{
-            visible: true,
-            type: 'spider',
-            formatter: (v) => `${(v.percent * 100).toFixed(0)}%`,
-            style: {
-              fill: '#86909C',
-              fontSize: 14,
-            },
-          }}
-          legend={{
-            position: 'bottom',
-          }}
-          statistic={{
-            title: {
-              style: {
-                fontSize: '14px',
-                lineHeight: 2,
-                color: 'rgb(--var(color-text-1))',
-              },
-              formatter: () => '内容量',
-            },
-            content: {
-              style: {
-                fontSize: '16px',
-                color: 'rgb(--var(color-text-1))',
-              },
-              formatter: (_, data) => {
-                const sum = data.reduce((a, b) => a + b.count, 0);
-                return Number(sum).toLocaleString();
-              },
-            },
-          }}
-        />
+        {/*<DonutChart*/}
+        {/*  autoFit*/}
+        {/*  height={340}*/}
+        {/*  data={data}*/}
+        {/*  radius={0.7}*/}
+        {/*  innerRadius={0.65}*/}
+        {/*  angleField="count"*/}
+        {/*  colorField="type"*/}
+        {/*  color={['#21CCFF', '#313CA9', '#249EFF']}*/}
+        {/*  interactions={[*/}
+        {/*    {*/}
+        {/*      type: 'element-single-selected',*/}
+        {/*    },*/}
+        {/*  ]}*/}
+        {/*  tooltip={{ showMarkers: false }}*/}
+        {/*  label={{*/}
+        {/*    visible: true,*/}
+        {/*    type: 'spider',*/}
+        {/*    formatter: (v) => `${(v.percent * 100).toFixed(0)}%`,*/}
+        {/*    style: {*/}
+        {/*      fill: '#86909C',*/}
+        {/*      fontSize: 14,*/}
+        {/*    },*/}
+        {/*  }}*/}
+        {/*  legend={{*/}
+        {/*    position: 'bottom',*/}
+        {/*  }}*/}
+        {/*  statistic={{*/}
+        {/*    title: {*/}
+        {/*      style: {*/}
+        {/*        fontSize: '14px',*/}
+        {/*        lineHeight: 2,*/}
+        {/*        color: 'rgb(--var(color-text-1))',*/}
+        {/*      },*/}
+        {/*      formatter: () => '内容量',*/}
+        {/*    },*/}
+        {/*    content: {*/}
+        {/*      style: {*/}
+        {/*        fontSize: '16px',*/}
+        {/*        color: 'rgb(--var(color-text-1))',*/}
+        {/*      },*/}
+        {/*      formatter: (_, data) => {*/}
+        {/*        const sum = data.reduce((a, b) => a + b.count, 0);*/}
+        {/*        return Number(sum).toLocaleString();*/}
+        {/*      },*/}
+        {/*    },*/}
+        {/*  }}*/}
+        {/*/>*/}
       </Spin>
     </Card>
   );
--- src/pages/dashboard/workplace/index.tsx
+++ src/pages/dashboard/workplace/index.tsx
@@ -17,37 +17,37 @@ const gutter = 16;
 function Workplace() {
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
-        <div className={styles.panel}>
-          <Overview />
-        </div>
-        <Row style={{ marginTop: gutter }}>
-          <Col
-            flex={1}
-            className={styles.panel}
-            style={{ marginRight: gutter }}
-          >
-            <PopularContents />
-          </Col>
-          <Col flex={1} className={styles.panel}>
-            <ContentPercentage />
-          </Col>
-        </Row>
-      </div>
-      <div className={styles.right}>
-        <div className={styles.panel}>
-          <Shortcuts />
-        </div>
-        <div className={styles.panel} style={{ marginTop: gutter }}>
-          <Carousel />
-        </div>
-        <div className={styles.panel} style={{ marginTop: gutter }}>
-          <Announcement />
-        </div>
-        <div className={styles.panel} style={{ marginTop: gutter }}>
-          <Docs />
-        </div>
-      </div>
+      {/*<div className={styles.left}>*/}
+      {/*  <div className={styles.panel}>*/}
+      {/*    <Overview />*/}
+      {/*  </div>*/}
+      {/*  <Row style={{ marginTop: gutter }}>*/}
+      {/*    <Col*/}
+      {/*      flex={1}*/}
+      {/*      className={styles.panel}*/}
+      {/*      style={{ marginRight: gutter }}*/}
+      {/*    >*/}
+      {/*      <PopularContents />*/}
+      {/*    </Col>*/}
+      {/*    <Col flex={1} className={styles.panel}>*/}
+      {/*      <ContentPercentage />*/}
+      {/*    </Col>*/}
+      {/*  </Row>*/}
+      {/*</div>*/}
+      {/*<div className={styles.right}>*/}
+      {/*  <div className={styles.panel}>*/}
+      {/*    <Shortcuts />*/}
+      {/*  </div>*/}
+      {/*  <div className={styles.panel} style={{ marginTop: gutter }}>*/}
+      {/*    <Carousel />*/}
+      {/*  </div>*/}
+      {/*  <div className={styles.panel} style={{ marginTop: gutter }}>*/}
+      {/*    <Announcement />*/}
+      {/*  </div>*/}
+      {/*  <div className={styles.panel} style={{ marginTop: gutter }}>*/}
+      {/*    <Docs />*/}
+      {/*  </div>*/}
+      {/*</div>*/}
     </div>
   );
 }
