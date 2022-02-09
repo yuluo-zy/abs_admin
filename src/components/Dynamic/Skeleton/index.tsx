a6c00c22c9df5cf2616ea73a14b77057ca278a65
liyu <liyu@espressif.com>
2022-02-09 18:01:05 +0800

    add: 动态骨架图

--- src/components/Dynamic/Skeleton/index.tsx
+++ src/components/Dynamic/Skeleton/index.tsx
@@ -2,18 +2,16 @@ import { Skeleton } from '@arco-design/web-react';
 import React, { useEffect, useState } from 'react';
 
 function DynamicSkeleton(props) {
+
   const [lazy, setLazy] = useState(true);
 
   useEffect(() => {
-    const creatInt = setTimeout(() => {
+    setTimeout(() => {
       setLazy(false);
     }, 700);
-    return () => {
-      clearInterval(creatInt);
-    };
   }, []);
 
-  return <Skeleton {...props} loading={lazy} />;
+  return <Skeleton  {...props} loading={lazy} />;
 }
 
 export default DynamicSkeleton;
