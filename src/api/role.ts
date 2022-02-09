5f2ec56939878801dcd2e6f97e3cef86834a6bee
liyu <liyu@espressif.com>
2022-02-09 15:18:23 +0800

    add: 角色api

--- src/api/role.ts
+++ src/api/role.ts
@@ -1,5 +1,18 @@
-import { httpGet } from '@/utils/httpRequest';
+import { httpDelete, httpGet, httpPost, httpPut } from '@/utils/httpRequest';
 
 export const getRole = () => {
   return httpGet('/role');
 };
+
+export const postRole = (data) => {
+  return httpPost('/role', data);
+};
+
+export const putRole = (data) => {
+  return httpPut('/role', data);
+};
+
+export const deleteRole = (id) => {
+  return httpDelete('/role/' + id);
+};
+
