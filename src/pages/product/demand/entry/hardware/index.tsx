c376feaf8f8276f421521fe4bb1f9b2a6cef4283
liyu <liyu@espressif.com>
2022-02-18 17:15:48 +0800

    add: 列表可拖动

--- src/pages/product/demand/entry/hardware/index.tsx
+++ src/pages/product/demand/entry/hardware/index.tsx
@@ -5,89 +5,107 @@ import useLocale from '@/pages/product/demand/locale/useLocale';
 import { getProductionInfo } from '@/api/production';
 import { Table } from '@arco-design/web-react';
 import DynamicSkeleton from '@/components/Dynamic/Skeleton';
+import ResizableTitle from '@/components/Dynamic/Resizeable';
 
-const columns = [
+const bodyCellStyle = {};
+const originColumns = [
 
   {
     title: 'Name',
     dataIndex: 'name',
+    bodyCellStyle: bodyCellStyle,
     fixed: 'left' as const,
     width: 120
   },
   {
     title: 'MPN',
     dataIndex: 'mpn',
-    width: 140
+    width: 140,
+    bodyCellStyle: bodyCellStyle
   },
   {
     title: 'Marketing Status',
     dataIndex: 'status',
+    bodyCellStyle: bodyCellStyle,
     width: 140
   },
   {
     title: 'Type',
     dataIndex: 'type',
+    bodyCellStyle: bodyCellStyle,
     width: 70
   }
   ,
   {
     title: 'Wi-Fi',
     dataIndex: 'wifi',
+    bodyCellStyle: bodyCellStyle,
     width: 200
   },
   {
     title: 'Bluetooth',
     dataIndex: 'bluetooth',
+    bodyCellStyle: bodyCellStyle,
     width: 200
   },
   {
     title: 'Temp (°C)',
     dataIndex: 'operatingTemp',
+    bodyCellStyle: bodyCellStyle,
     width: 100
   },
   {
     title: 'Gpio',
     dataIndex: 'gpio',
+    bodyCellStyle: bodyCellStyle,
     width: 50
   },
   {
     title: 'Flash (MB)',
     dataIndex: 'flash',
+    bodyCellStyle: bodyCellStyle,
     width: 60
   },
   {
     title: 'SRAM (KB)',
     dataIndex: 'sram',
+    bodyCellStyle: bodyCellStyle,
     width: 60
   },
   {
     title: 'PSRAM (MB)',
     dataIndex: 'psram',
+    bodyCellStyle: bodyCellStyle,
     width: 60
   },
   {
     title: 'ROM (KB)',
     dataIndex: 'rom',
+    bodyCellStyle: bodyCellStyle,
     width: 60
   },
   {
     title: 'Freq. (MHz)',
     dataIndex: 'freq',
+    bodyCellStyle: bodyCellStyle,
     width: 60
   },
   {
     title: 'Size (mm)',
     dataIndex: 'dimensions',
+    bodyCellStyle: bodyCellStyle,
     width: 120
   },
   {
     title: 'MPQ/SPQ',
     dataIndex: 'spq',
+    bodyCellStyle: bodyCellStyle,
     width: 70
   },
   {
     title: 'MOQ',
     dataIndex: 'moq',
+    bodyCellStyle: bodyCellStyle,
     width: 70
   }
 ];
@@ -98,6 +116,39 @@ export default function HardwareSelection() {
   useEffect(() => {
     fetchProductionList();
   }, []);
+  const [columns, setColumns] = useState(
+    originColumns.map((column, index) => {
+      if (column.width) {
+        return {
+          ...column,
+          onHeaderCell: col => ({
+            width: col.width,
+            onResize: handleResize(index)
+          })
+        };
+      }
+      return column;
+    })
+  );
+
+  function handleResize(index) {
+    return (e, { size }) => {
+      setColumns(prevColumns => {
+        const nextColumns = [...prevColumns];
+        nextColumns[index] = {
+          ...nextColumns[index],
+          width: size.width
+        };
+        return nextColumns;
+      });
+    };
+  }
+
+  const components = {
+    header: {
+      th: ResizableTitle
+    }
+  };
 
   function fetchProductionList() {
     getProductionInfo()
@@ -107,16 +158,19 @@ export default function HardwareSelection() {
   }
 
   const t = useLocale();
+
   return <DynamicCard>
     <DynamicOuterCard title={t['hardware.production.info.title']}>
       <div><p>kjhkjhkjh</p></div>
     </DynamicOuterCard>
     <DynamicOuterCard>
-      <DynamicSkeleton text={{ rows: 10, width: '60rem' }}>
+      <DynamicSkeleton text={{ rows: 10, width: '90rem' }}>
         <div>
           <Table
             scroll={{ x: true, y: 600 }}
             border
+            borderCell
+            components={components}
             size={'mini'}
             columns={columns}
             data={productList}
