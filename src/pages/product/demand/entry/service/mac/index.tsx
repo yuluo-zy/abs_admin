dc0d486dce589f0a8ef328b03de9a643550c3161
yuluo <yuluozy@outlook.com>
2022-05-10 19:39:55 +0800

    step4 表单内容提交校验

--- src/pages/product/demand/entry/service/mac/index.tsx
+++ src/pages/product/demand/entry/service/mac/index.tsx
@@ -20,155 +20,208 @@ import ProductStore from "@/store/product";
 import shallow from "zustand/shallow";
 import { convertToNumber, setColon } from "@/utils/stringTools";
 import { IconArrowRight } from "@arco-design/web-react/icon";
-import DynamicMark from "@/components/Dynamic/tag/mark";
-import DynamicRules from "@/components/Dynamic/rules";
 import { postMacCustomDemand } from "@/api/demand";
 
+const FormItem = Form.Item;
 const bodyStyle = {
-  padding: '3rem'
+  padding: "3rem"
 };
-export function MacInput(props) {
-  return(
-    <Input {...props}/>
-  )
-}
+
 
 export const getMac = (data, oldData) => {
   return setColon(data
     .toUpperCase()
     .split("")
-    .filter(value => value.length === 1 && value.match(/[0-9A-F]/)), data.length < oldData.length)
-}
+    .filter(value => value.length === 1 && value.match(/[0-9A-F]/)), data.length < oldData.length);
+};
 export default function CustomMac() {
+  const [form] = Form.useForm();
   const t = useLocale();
   const [macData, setMacData] = ProductStore(state => [state.macData, state.setMacData], shallow);
   const demandId = ProductStore(state => state.demandId);
   const getMacStartInfo = (value) => {
+    const temp = getMac(value, macData?.macStart || "");
     setMacData({
-      macStart: getMac(value, macData?.macStart || '')
-    })
-  }
+      macStart: temp
+    });
+    form.setFieldValue("macStart", temp);
+  };
   const getMacEndInfo = (value) => {
+    const temp = getMac(value, macData?.macEnd || "");
     setMacData({
-      macEnd: getMac(value, macData?.macEnd || '')
-    })
-  }
+      macEnd: temp
+    });
+    form.setFieldValue("macEnd", temp);
+  };
 
-  const getMacNumber = (start, end)=> {
-    return (convertToNumber(end) - convertToNumber(start)).toLocaleString('en-US')
-  }
-  const getProjectNumber = (start, end, project)=> {
-    return Math.floor((convertToNumber(end) - convertToNumber(start) )/ project * 0.998).toLocaleString('en-US')
-  }
+  const getMacNumber = (start, end) => {
+    return (convertToNumber(end) - convertToNumber(start)).toLocaleString("en-US");
+  };
+  const getProjectNumber = (start, end, project) => {
+    return Math.floor((convertToNumber(end) - convertToNumber(start)) / project * 0.998).toLocaleString("en-US");
+  };
 
   const postMacCustom = () => {
+    try {
+      form.validate();
+    } catch (error) {
+      return;
+    }
+    setMacData({
+      ...form.getFieldsValue()
+    });
     postMacCustomDemand({
       ...macData,
       demandId: demandId
-    }).then( res => {
-      if(res.data.success){
+    }).then(res => {
+      if (res.data.success) {
         Message.success(t["submit.hardware.success"]);
       }
-    })
-  }
+    });
+  };
 
-  return (<DynamicOuterCard title={t['firmware.mac.title']} bodyStyle={bodyStyle}>
-    <DynamicSkeleton animation text={{ rows: 10, width: ['100%', 600, 400] }}>
-      <div className={style['context']}>
-        <Alert type='warning' className={style['context-text']} closable
-               content={t['firmware.mac.partitions.write.area.efuse']}
-               style={{ maxWidth: '30rem', marginBottom: 20 }} closeElement='Close' />
-        <Space size={15} align={'start'}>
-          <Typography.Text><DynamicMark/>{t['firmware.mac.partitions.write.area']}</Typography.Text>
-          <Space size={40}>
-            <DynamicRadioGroup direction='vertical'
+  return (<DynamicOuterCard title={t["firmware.mac.title"]} bodyStyle={bodyStyle}>
+    <DynamicSkeleton animation text={{ rows: 10, width: ["100%", 600, 400] }}>
+      <Form
+        scrollToFirstError
+        form={form}
+        initialValues={{ ...macData }}
+        onSubmit={postMacCustom}
+      >
+
+        <div className={style["context"]}>
+          <Alert type="warning" className={style["context-text"]} closable
+                 content={t["firmware.mac.partitions.write.area.efuse"]}
+                 style={{ maxWidth: "30rem", marginBottom: 20 }} closeElement="Close" />
+          <FormItem label={t["firmware.mac.partitions.write.area"]}
+                    labelCol={{ span: 4 }}
+                    labelAlign={"left"}
+                    field="type"
+                    rules={[
+                      {
+                        required: true,
+                        message: t["firmware.mac.partitions.write.area.error"]
+                      }
+                    ]}
+          >
+            <DynamicRadioGroup direction="vertical"
                                defaultValue={macData?.type}
                                options={[{
-                                 label: 'Flash',
-                                 value: 'FLASH'
+                                 label: "Flash",
+                                 value: "FLASH"
                                }, {
-                                 label: 'efuse',
-                                 value: 'EFUSE'
+                                 label: "efuse",
+                                 value: "EFUSE"
                                }]}
-                               onChange={(value) => setMacData({
-                                 type: value
-                               })}
-            />
-          </Space>
+                               onChange={(value) => {
+                                 form.setFieldValue("type", value);
+                                 setMacData({
+                                   type: value
+                                 });
+                               }}
 
+            />
+          </FormItem>
           {
-            macData?.type === 'FLASH' && <Space style={{marginLeft: '4rem'}}>
-              <Typography.Text><DynamicMark/>{t['firmware.mac.partitions.flash.write.area']}</Typography.Text>
+            macData?.type === "FLASH" &&
+            <FormItem label={t["firmware.mac.partitions.flash.write.area"]}
+                      field={"offsetAddr"}
+                      labelCol={{ span: 4 }}
+                      labelAlign={"left"}
+                      rules={[
+                        {
+                          required: true,
+                          message: t["firmware.mac.partitions.flash.write.area.error"],
+                          validator: (value, callback) => {
+                            if ((value === undefined || value === "") && macData?.type === "FLASH") {
+                              callback(t["firmware.mac.partitions.flash.write.area.error"]);
+                            }
+                          }
+                        }
+                      ]}>
               <Input
                 style={{ width: 300 }}
-                defaultValue={macData?.offsetAddr}
-                onChange={value => {
-                  setMacData({
-                    offsetAddr: value
-                  });
-                }}
                 placeholder="Please Enter Offset Address"
               />
-            </Space>
+            </FormItem>
           }
-        </Space>
-      </div>
-      <Divider style={{ borderBottomStyle: 'dashed' }} />
-      <Space size={20}>
-        <Typography.Text><DynamicMark/>{t['firmware.mac.partitions.flash.write.area.mac']}</Typography.Text>
-        <InputNumber
-          style={{ width: 300 }}
-          mode='button'
-          min={1}
-          max={4}
-          defaultValue={macData?.macNumPerProduction}
-          onChange={value => {
-            setMacData({
-              macNumPerProduction: value
-            });
-          }}
-          placeholder="Please Enter Mac Numbers"
-        />
-      </Space>
-      <Divider style={{ borderBottomStyle: 'dashed' }} />
-      <Space size={20} direction={'vertical'}>
-        <Typography.Title heading={5} type='primary'>
-          {t['firmware.mac.partitions.flash.write.area.title']}
-        </Typography.Title>
+        </div>
+        <Divider style={{ borderBottomStyle: "dashed" }} />
+        <FormItem label={t["firmware.mac.partitions.flash.write.area.mac"]}
+                  labelCol={{ span: 5 }}
+                  labelAlign={"left"}
+                  field={"macNumPerProduction"}
+                  rules={[{ required: true, type: "number", min: 1, max: 4 }]}>
+          <InputNumber
+            mode="button"
+            style={{ width: "10rem" }}
+            min={1}
+            max={4}
+            placeholder="Please Enter Mac Numbers"
+            onChange={(value) => {
+              form.setFieldValue("macNumPerProduction", value);
+              setMacData({
+                macNumPerProduction: value
+              });
+            }}
+          />
+        </FormItem>
+        <Divider style={{ borderBottomStyle: "dashed" }} />
+        <Space size={20} direction={"vertical"}>
+          <Typography.Title heading={5} type="primary">
+            {t["firmware.mac.partitions.flash.write.area.title"]}
+          </Typography.Title>
+          <FormItem label={t["firmware.mac.partitions.start"]}
+                    field={"macStart"}
+                    labelCol={{ span: 3 }}
+                    labelAlign={"left"}
+                    rules={[
+                      {
+                        required: true,
+                        message: t["firmware.mac.partitions.start.error"],
+                        length: 17
+                      }
+                    ]}>
+            <Input value={macData?.macStart} onChange={getMacStartInfo} style={{ maxWidth: "20rem" }} />
+          </FormItem>
 
-        <Space size={40}>
-          <Typography.Text>
-            <DynamicMark/>
-            {t['firmware.mac.partitions.start']}</Typography.Text>
-          <MacInput value={macData?.macStart} onChange={getMacStartInfo}/>
+          <FormItem label={t["firmware.mac.partitions.end"]}
+                    field={"macEnd"}
+                    labelCol={{ span: 3 }}
+                    labelAlign={"left"} rules={[
+            {
+              required: true,
+              message: t["firmware.mac.partitions.end.error"],
+              length: 17
+            }
+          ]}
+          >
+            <Input value={macData?.macEnd} onChange={getMacEndInfo} style={{ maxWidth: "20rem" }} />
+          </FormItem>
+          <br />
+          <Space size={10}>
+            <Typography.Text>{t["firmware.mac.partitions.info1"]}</Typography.Text>
+            <Tag color="arcoblue" checkable={false}>{getMacNumber(macData?.macStart, macData?.macEnd)}</Tag>
+            <Typography.Text>{t["firmware.mac.partitions.info2"]}</Typography.Text>
+            <Tag color="arcoblue"
+                 checkable={false}>{getProjectNumber(macData?.macStart, macData?.macEnd, macData?.macNumPerProduction)}</Tag>
+            <Typography.Text>{t["firmware.mac.partitions.info3"]}</Typography.Text>
+          </Space>
         </Space>
-        <Space size={40}>
-          <Typography.Text><DynamicMark/>{t['firmware.mac.partitions.end']}</Typography.Text>
-          <DynamicRules>
-          <MacInput value={macData?.macEnd} onChange={getMacEndInfo} />
-          </DynamicRules>
-        </Space>
-        <br />
-        <Space size={10}>
-          <Typography.Text>{t['firmware.mac.partitions.info1']}</Typography.Text>
-          <Tag color='arcoblue' checkable={false}>{getMacNumber(macData?.macStart,macData?.macEnd )}</Tag>
-          <Typography.Text>{t['firmware.mac.partitions.info2']}</Typography.Text>
-          <Tag color='arcoblue' checkable={false}>{getProjectNumber(macData?.macStart,macData?.macEnd, macData.macNumPerProduction )}</Tag>
-          <Typography.Text>{t['firmware.mac.partitions.info3']}</Typography.Text>
-        </Space>
-      </Space>
-      <Alert className={style['context-warn']} type='warning' closable content={t['firmware.mac.partitions.warn']}
-             style={{ maxWidth: '30rem', marginBottom: 20 }} closeElement='Close' />
-      <Divider style={{ borderBottomStyle: 'dashed' }} />
-      <div className={style["context-next"]}>
+        <Alert className={style["context-warn"]} type="warning" closable content={t["firmware.mac.partitions.warn"]}
+               style={{ maxWidth: "30rem", marginBottom: 20 }} closeElement="Close" />
+        <Divider style={{ borderBottomStyle: "dashed" }} />
+        <div className={style["context-next"]}>
           <Button type="primary"
                   size={"large"}
+                  htmlType="submit"
                   icon={<IconArrowRight />}
-                  onClick={postMacCustom}
+
           >
             {t["hardware.production.info.next"]}
           </Button>
-      </div>
+        </div>
+      </Form>
     </DynamicSkeleton>
   </DynamicOuterCard>);
 }
