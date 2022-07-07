7013b405816441bc520c7d8952fd879835418b95
liyu <liyu@espressif.com>
2022-07-07 14:41:50 +0800

    add: 预适配阶段完成

--- src/pages/product/demand/entry/service/pre-fit/index.tsx
+++ src/pages/product/demand/entry/service/pre-fit/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import DynamicOuterCard from '@/components/Dynamic/Card/outer-frame';
+import React from "react";
+import DynamicOuterCard from "@/components/Dynamic/Card/outer-frame";
 import {
   Alert,
   Button,
@@ -11,19 +11,20 @@ import {
   Select,
   Space,
   Table,
+  Tag,
   Typography
-} from '@arco-design/web-react';
-import useLocale from '@/pages/product/demand/locale/useLocale';
-import DynamicRadioGroup from '@/components/Dynamic/Radio';
-import style from './style/index.module.less';
-import DynamicSkeleton from '@/components/Dynamic/Skeleton';
-import { ProductStore } from '@/store/product';
-import shallow from 'zustand/shallow';
-import { getMac } from '@/utils/stringTools';
-import { IconArrowRight } from '@arco-design/web-react/icon';
-import { postAdaptCustomDemand } from '@/api/demand';
-import { getNextRouter } from '@/utils/getNext';
-import { useHistory } from 'react-router';
+} from "@arco-design/web-react";
+import useLocale from "@/pages/product/demand/locale/useLocale";
+import DynamicRadioGroup from "@/components/Dynamic/Radio";
+import style from "./style/index.module.less";
+import DynamicSkeleton from "@/components/Dynamic/Skeleton";
+import { ProductStore } from "@/store/product";
+import shallow from "zustand/shallow";
+import { getMac } from "@/utils/stringTools";
+import { IconArrowRight } from "@arco-design/web-react/icon";
+import { postAdaptCustomDemand } from "@/api/demand";
+import { getNextRouter } from "@/utils/getNext";
+import { useHistory } from "react-router";
 
 const Option = Select.Option;
 const FormItem = Form.Item;
@@ -51,7 +52,8 @@ export default function PreFit() {
   const t = useLocale();
   const history = useHistory();
   const [fitData, setFitData] = ProductStore(state => [state.fitData, state.setFitData], shallow);
-  const [demandId, serviceType] = ProductStore(state => [state.demandId, state.serviceType], shallow);
+  const [demandId, serviceType, serviceData] = ProductStore(state =>
+    [state.demandId, state.serviceType, state.serviceData], shallow);
 
   const postFitCustom = () => {
     try {
@@ -126,6 +128,13 @@ export default function PreFit() {
     setValue("commonName", temp);
   };
 
+  const getDay = (day) => {
+    if(day !== undefined){
+      return day * 365
+    }
+    return 0
+  }
+
   return (<DynamicOuterCard title={t["firmware.pre.title"]} bodyStyle={bodyStyle}>
     <DynamicSkeleton animation text={{ rows: 10, width: ["100%", 600, 400] }}>
       <Form
@@ -147,129 +156,137 @@ export default function PreFit() {
                     }
                   ]}
         >
-          <Input style={maxWidth} />
+          <Input style={maxWidth} maxLength={35}/>
         </FormItem>
 
         <Divider style={{ borderBottomStyle: "dashed" }} />
         <b>{t["firmware.pre.ca.setting.config.title"]}</b>
         <br />
-        <FormItem label={t["firmware.pre.ca.setting.config.type"]}
-                  labelAlign={"left"}
-                  field={"isAdapt"}
-                  rules={[{ required: true, message: t["firmware.pre.ca.setting.config.type.error"] }]}>
+        <div className={style['ca']}>
+          <FormItem label={t["firmware.pre.ca.setting.config.type"]}
+                    labelAlign={"left"}
+                    field={"isAdapt"}
+                    labelCol={{ span: 7 }}
+                    wrapperCol={{ span: 17 }}
+                    className={style['ca-way']}
+                    rules={[{ required: true, message: t["firmware.pre.ca.setting.config.type.error"] }]}>
 
-          <DynamicRadioGroup direction="vertical"
-                             defaultValue={fitData?.isAdapt}
-                             options={[{
-                               label: t["firmware.pre.ca.setting.config.type.custom"],
-                               value: 1
-                             }, {
-                               label: t["firmware.pre.ca.setting.config.type.no.custom"],
-                               value: 0
-                             }]}
-                             onChange={(value) => {
-                               setValue("isAdapt", value);
-                               if(value === 0) {
-                                 setValue("transmissionMethod", null)
-                               }
-                             }}
-          />
-          {fitData?.isAdapt === 0 && <Alert className={style["context-text"]} closable
-                                            content={t["firmware.pre.ca.setting.config.type.no.custom.info"]}
-                                            style={{ maxWidth: "30rem", marginBottom: 20, marginTop:"2rem" }} closeElement="Close" />}
-          {fitData?.transmissionMethod === "PGP" && <Alert className={style["context-text"]} closable
+            <DynamicRadioGroup direction="vertical"
+                               defaultValue={fitData?.isAdapt}
+                               options={[{
+                                 label: t["firmware.pre.ca.setting.config.type.custom"],
+                                 value: 1
+                               }, {
+                                 label: t["firmware.pre.ca.setting.config.type.no.custom"],
+                                 value: 0
+                               }]}
+                               onChange={(value) => {
+                                 setValue("isAdapt", value);
+                                   setValue("transmissionMethod", undefined)
+                               }}
+            />
+          </FormItem>
+          {fitData?.isAdapt === 1 &&
+            <FormItem label={t["firmware.pre.ca.setting.config.type.transmission.method"]}
+                      labelAlign={"left"}
+                      field={"transmissionMethod"}
+                      labelCol={{ span: 7 }}
+                      wrapperCol={{ span: 17 }}
+                      className={style['ca-transmission']}
+                      rules={[{
+                        required: true,
+                        message: t["firmware.pre.ca.setting.config.type.transmission.method.error"],
+                        validator: (value, callback) => {
+                          if ((value === undefined || value === "") && fitData?.isAdapt === 1) {
+                            callback(t["firmware.pre.ca.setting.config.type.transmission.method.error"]);
+                          }
+                        }
+                      }]}>
+
+              <DynamicRadioGroup direction="vertical"
+                                 defaultValue={fitData?.transmissionMethod}
+                                 options={[{
+                                   label: "PGP",
+                                   value: "PGP"
+                                 }, {
+                                   label: "YUBIKEY",
+                                   value: "YUBIKEY"
+                                 }]}
+                                 onChange={(value) => {
+                                   setValue("transmissionMethod", value);
+                                 }}
+              />
+            </FormItem>}
+          {fitData?.isAdapt === 0 && <Alert className={style["ca-text"]}
+                                            content={t["firmware.pre.ca.setting.config.type.no.custom.info"]} closeElement="Close" />}
+          {fitData?.transmissionMethod === "PGP" && <Alert className={style["ca-text"]}
                                                            content={t["firmware.pre.ca.setting.config.type.no.custom.info.PGP"]}
-                                                           style={{ maxWidth: "30rem", marginBottom: 20 }}
                                                            closeElement="Close" />}
-          {fitData?.transmissionMethod === "YUBIKEY" && <Alert className={style["context-text"]} closable
+          {fitData?.transmissionMethod === "YUBIKEY" && <Alert className={style["ca-text"]}
                                                                content={t["firmware.pre.ca.setting.config.type.no.custom.info.YUBIKEY"]}
-                                                               style={{ maxWidth: "30rem", marginBottom: 20 }}
                                                                closeElement="Close" />}
-
-        </FormItem>
-        <br /><br />
-        {fitData?.isAdapt === 1 &&
-          <FormItem label={t["firmware.pre.ca.setting.config.type.transmission.method"]}
-                                             labelAlign={"left"}
-                                             field={"transmissionMethod"}
-                                             rules={[{
-                                               required: false,
-                                               message: t["firmware.pre.ca.setting.config.type.transmission.method.error"]
-                                             }]}>
-
-          <DynamicRadioGroup direction="vertical"
-                             defaultValue={fitData?.transmissionMethod}
-                             options={[{
-                               label: "PGP",
-                               value: "PGP"
-                             }, {
-                               label: "YUBIKEY",
-                               value: "YUBIKEY"
-                             }]}
-                             onChange={(value) => {
-                               setValue("transmissionMethod", value);
-                             }}
-          />
-        </FormItem>}
-
+        </div>
 
         <Divider style={{ borderBottomStyle: "dashed" }} />
-        <b>EFUSE_KEY_BLOCK & Storage type of Device Certificate</b>
-        <br />
-        <br />
-        <Space size={30} className={style["centered"]}>
-          <Typography.Text>
-            {t["firmware.pre.ca.setting.config.type.transmission.EFUSE_KEY_BLOCKx"]}
-          </Typography.Text>
-          <FormItem
-            field={"leaveBlock"}
-          >
-            <Select
-              placeholder={t["firmware.pre.ca.setting.config.type.Leave"]}
-              style={{ width: 400 }}
-              defaultValue={fitData?.leaveBlock}
-              onChange={(value) => setValue("leaveBlock", value)}
+        {serviceData.storageType === 1 && <div>
+          <b>EFUSE_KEY_BLOCK & Storage type of Device Certificate</b>
+          <br />
+          <br/>
+          <Space size={30} className={style["centered"]}>
+            <Typography.Text>
+              {t["firmware.pre.ca.setting.config.type.transmission.EFUSE_KEY_BLOCKx"]}
+            </Typography.Text>
+            <FormItem
+              field={"leaveBlock"}
             >
-              {options.map((option, index) => (
-                <Option key={option} value={option}>
-                  {option}
-                </Option>
-              ))}
-            </Select>
-          </FormItem>
-        </Space>
-        <br />
-        <br />
-        <Space size={30} className={style["centered"]}>
-          <Typography.Text>
-            {t["firmware.pre.ca.setting.config.type.device"]}
-          </Typography.Text>
-          <FormItem
-            field={"certificatiesPlace"}
-          >
-            <Select
-              placeholder={t["firmware.pre.ca.setting.config.type.Leave"]}
-              style={{ width: 300 }}
-              defaultValue={fitData?.certificatiesPlace}
-              onChange={(value) => setValue("certificatiesPlace", value)}
+              <Select
+                placeholder={t["firmware.pre.ca.setting.config.type.Leave"]}
+                style={{width: 400}}
+                defaultValue={fitData?.leaveBlock}
+                onChange={(value) => setValue("leaveBlock", value)}
+              >
+                {options.map((option, index) => (
+                  <Option key={option} value={option}>
+                    {option}
+                  </Option>
+                ))}
+              </Select>
+            </FormItem>
+          </Space>
+          <br />
+          <br />
+          <Space size={30} className={style["centered"]}>
+            <Typography.Text>
+              {t["firmware.pre.ca.setting.config.type.device"]}
+            </Typography.Text>
+            <FormItem
+              field={"certificatiesPlace"}
             >
-              {certificaties.map((option, index) => (
-                <Option key={option} value={option}>
-                  {option}
-                </Option>
-              ))}
-            </Select>
-          </FormItem>
-        </Space>
+              <Select
+                placeholder={t["firmware.pre.ca.setting.config.type.Leave"]}
+                style={{ width: 300 }}
+                defaultValue={fitData?.certificatiesPlace}
+                onChange={(value) => setValue("certificatiesPlace", value)}
+              >
+                {certificaties.map((option, index) => (
+                  <Option key={option} value={option}>
+                    {option}
+                  </Option>
+                ))}
+              </Select>
+            </FormItem>
+          </Space>
 
-        <Divider style={{ borderBottomStyle: "dashed" }} />
+          <Divider style={{ borderBottomStyle: "dashed" }} />
+        </div>}
+
         <b>{t["firmware.pre.ca.setting.config.device.certificates"]}</b>
         <br />
         <br />
         <FormItem label={"subject"}
                   field={"subject"}
                   labelAlign={"left"}
-                  extra={"C=CN,ST=SH,O=Espressif"}
+                  extra={"Defaults: C=CN,ST=SH,O=Espressif"}
                   rules={[
                     {
                       required: true,
@@ -285,7 +302,7 @@ export default function PreFit() {
         <FormItem label={"commonName: "}
                   field={"commonName"}
                   labelAlign={"left"}
-                  extra={"Upcase MAC address e.g: AABBCC112233"}
+                  extra={"Defaults: Upcase MAC address e.g: AABBCC112233"}
                   rules={[
                     {
                       required: true,
@@ -298,18 +315,26 @@ export default function PreFit() {
         <FormItem label={"validityDate: "}
                   field={"validityDate"}
                   labelAlign={"left"}
-                  extra={"5475s (15 years)"}
+                  extra={"Defaults: 5475 Day (15 years)"}
                   rules={[
                     {
                       required: true,
                       message: "Validity Date value cannot be empty"
                     }
                   ]}>
-          <InputNumber value={fitData?.validityDate} onChange={
-            (value) => {
-              setValue("validityDate", value);
-            }
-          } style={{ maxWidth: "20rem", margin: "1rem", marginLeft:0 }} />
+          <div className={style['year']}>
+            <InputNumber max={30} value={fitData?.validityDate} onChange={
+              (value) => {
+                setValue("validityDate", value);
+              }
+            } style={{ maxWidth: "5rem", margin: "1rem", marginLeft:0 }} />
+
+            <b>Year</b>
+            <p>=</p>
+            <Tag color={'blue'} style={{margin: '1rem'}}>{getDay(fitData?.validityDate)}</Tag>
+            <b>Day</b>
+          </div>
+
         </FormItem>
 
         <Divider style={{ borderBottomStyle: "dashed" }} />
