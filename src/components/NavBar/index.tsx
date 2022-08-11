c864baf9e6164c7c4011dd0097602fe225b1265f
liyu <liyu@espressif.com>
2022-08-11 17:49:35 +0800

    add: 删除对应的无效模块

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import {
   Avatar,
   Button,
@@ -11,30 +11,23 @@ import {
   Tooltip
 } from "@arco-design/web-react";
 import {
-  IconDashboard,
-  IconExperiment,
-  IconInteraction,
   IconLanguage,
   IconMoonFill,
-  IconNotification,
   IconPoweroff,
   IconSettings,
   IconSunFill,
-  IconTag,
-  IconUser
+  IconTag
 } from "@arco-design/web-react/icon";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { GlobalState } from "@/store";
 import { GlobalContext } from "@/context";
 import useLocale from "@/utils/useHook/useLocale";
 import Logo from "@/assets/logo.svg";
-import MessageBox from "@/components/MessageBox";
 import IconButton from "./IconButton";
 import Settings from "../Settings";
 import styles from "./style/index.module.less";
 import defaultLocale from "@/locale";
 import useStorage from "@/utils/useHook/useStorage";
-import { generatePermission } from "@/routes";
 import { loginOut } from "@/api/login";
 import HelpInfo from "@/pages/help";
 import { setHelpKey } from "@/store/help";
@@ -42,10 +35,8 @@ import { setHelpKey } from "@/store/help";
 function Navbar({ show }: { show: boolean }) {
   const t = useLocale();
   const userInfo = useSelector((state: GlobalState) => state.userInfo);
-  const dispatch = useDispatch();
 
   const [_, setUserStatus] = useStorage("userStatus");
-  const [role, setRole] = useStorage("userRole", "admin");
 
   const get_avatar = (user_name: string): string => {
     const user_url = encodeURI(user_name);
@@ -76,17 +67,6 @@ function Navbar({ show }: { show: boolean }) {
     }
   }
 
-  useEffect(() => {
-    dispatch({
-      type: "update-userInfo",
-      payload: {
-        userInfo: {
-          ...userInfo,
-          permissions: generatePermission(role)
-        }
-      }
-    });
-  }, [role]);
 
   if (!show) {
     return (
@@ -100,54 +80,13 @@ function Navbar({ show }: { show: boolean }) {
     );
   }
 
-  const handleChangeRole = () => {
-    const newRole = role === "admin" ? "user" : "admin";
-    setRole(newRole);
-  };
 
   const droplist = (
     <Menu onClickMenuItem={onMenuItemClick}>
-      <Menu.SubMenu
-        key="role"
-        title={
-          <>
-            <IconUser className={styles["dropdown-icon"]} />
-            <span className={styles["user-role"]}>
-              {role === "admin"
-                ? t["menu.user.role.admin"]
-                : t["menu.user.role.user"]}
-            </span>
-          </>
-        }
-      >
-        <Menu.Item onClick={handleChangeRole} key="switch role">
-          <IconTag className={styles["dropdown-icon"]} />
-          {t["menu.user.switchRoles"]}
-        </Menu.Item>
-      </Menu.SubMenu>
       <Menu.Item key="setting">
         <IconSettings className={styles["dropdown-icon"]} />
         {t["menu.user.setting"]}
       </Menu.Item>
-      <Menu.SubMenu
-        key="more"
-        title={
-          <div style={{ width: 80 }}>
-            <IconExperiment className={styles["dropdown-icon"]} />
-            {t["message.seeMore"]}
-          </div>
-        }
-      >
-        <Menu.Item key="workplace">
-          <IconDashboard className={styles["dropdown-icon"]} />
-          {t["menu.dashboard.workplace"]}
-        </Menu.Item>
-        <Menu.Item key="card list">
-          <IconInteraction className={styles["dropdown-icon"]} />
-          {t["menu.list.cardList"]}
-        </Menu.Item>
-      </Menu.SubMenu>
-
       <Divider style={{ margin: "4px 0" }} />
       <Menu.Item key="logout">
         <IconPoweroff className={styles["dropdown-icon"]} />
@@ -205,11 +144,6 @@ function Navbar({ show }: { show: boolean }) {
               onClick={() => setHelpKey("1")}
             />
           </Tooltip>
-        </li>
-        <li>
-          <MessageBox>
-            <IconButton icon={<IconNotification />} />
-          </MessageBox>
         </li>
         <li>
           <Tooltip
