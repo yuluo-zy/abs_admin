f6ec7b023cdfcebd781674682bc994a04cba1475
liyu <liyu@espressif.com>
2022-08-15 10:57:07 +0800

    add: 修改菜单导入错误

--- src/pages/product/menu.tsx
+++ src/pages/product/menu.tsx
@@ -1,9 +1,12 @@
 import { ManageMenuProps } from "@/components/type";
 import { Button, Space } from "@arco-design/web-react";
+import React from "react";
 
 export default function DemandManageMenu(props: { menu: Array<ManageMenuProps> }) {
   const { menu } = props;
-  return (<Space>
-    {menu.map((item, index) => <Button key={index} type="primary" onClick={item.onChange}>{item.name}</Button>)}
-  </Space>);
+  return <Space>
+    {menu.map((item, index) =>
+      <Button key={index} type="primary" onClick={item.onChange}>{item.name}</Button>
+    )}
+  </Space>;
 }
