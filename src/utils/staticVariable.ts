558e2cacf1684260487e2db9d23e518421f5846d
liyu <liyu@espressif.com>
2022-08-22 14:19:41 +0800

    add: 添加权限字

--- src/utils/staticVariable.ts
+++ src/utils/staticVariable.ts
@@ -8,3 +8,38 @@ export const CUSTOM_NAME_PERMISSION = "CUSTOM_NAME_PERMISSION";
 export const CUSTOM_CODE_PERMISSION = "CUSTOM_CODE_PERMISSION";
 // 客户项目权限定义
 export const CUSTOM_PROJECT_PERMISSION = "CUSTOM_PROJECT_PERMISSION";
+
+// 客户取消需求
+export const DEMAND_CUSTOMER_CANCEL = "demand:customerCancel";
+// 客户撤回需求
+export const DEMAND_CUSTOMER_WITHDRAW = "demand:customerWithdraw";
+// 客户复制需求
+export const DEMAND_CUSTOMER_COPY = "demand:customerCopy";
+// 客户发起需求
+export const DEMAND_CUSTOMER_CREATE = "demand:customerCreate";
+// 商务进行需求接收
+export const DEMAND_BS_RECEIVE = "demand:bsReceive";
+// 商务驳回需求
+export const DEMAND_BS_REJECT = "demand:bsReject";
+
+// 工程师 进行需求取消
+export const DEMAND_ENGINEER_CANCEL = "demand:espEngineerCancel";
+// 工程师进行 需求撤回
+export const DEMAND_ENGINEER_WITHDRAW = "demand:espEngineerWithdraw";
+// 工程师进行需求复制
+export const DEMAND_ENGINEER_COPY = "demand:espEngineerCopy";
+// 工程师 发起需求
+export const DEMAND_ENGINEER_COMMIT = "demand:espEngineerCreate";
+// 工程师 接收验证需求
+export const DEMAND_ENGINEER_RECEIVE = "demand:espEngineerReceive";
+// 工程师 驳回请求
+export const DEMAND_ENGINEER_REJECT = "demand:espEngineerReject";
+// 工程师 派发需求
+export const DEMAND_ENGINEER_DISTRIBUTE = "demand:espEngineerDistribute";
+
+// 工厂 接收需求
+export const DEMAND_FACTORY_RECEIVE = "demand:factoryReceive";
+// 驳回 工厂需求
+export const DEMAND_FACTORY_REJECT = "demand:factoryReject";
+// 完成验证需求
+export const DEMAND_FACTORY_COMPLETE = "demand:factoryComplete";
