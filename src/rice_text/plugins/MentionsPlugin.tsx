9bb6c7774e337e91ff9a9b4cc17030e7e41da771
liyu <liyu@espressif.com>
2022-08-30 14:51:55 +0800

    fix: 添加对应对象的@功能

--- src/rice_text/plugins/MentionsPlugin.tsx
+++ src/rice_text/plugins/MentionsPlugin.tsx
@@ -5,7 +5,6 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import * as ReactDOM from "react-dom";
 import {
   LexicalTypeaheadMenuPlugin,
-  QueryMatch,
   TypeaheadOption,
   useBasicTypeaheadTriggerMatch
 } from "./LexicalTypeaheadMenuPlugin";
@@ -14,76 +13,13 @@ import "./styles/MentionsPlugin.less";
 import { getRelatable } from "@/api/demand";
 import { ProductStore } from "@/store/product";
 import shallow from "zustand/shallow";
-
-const PUNCTUATION =
-  "\\.,\\+\\*\\?\\$\\@\\|#{}\\(\\)\\^\\-\\[\\]\\\\/!%'\"~=<>_:;";
+import { IconUser } from "@arco-design/web-react/icon";
 
 interface UserInfo {
   id: number,
   username: string,
   email: string
 }
-
-const NAME = "\\b[A-Z][\u4E00-\u9FA5]{2,4}[^\\s" + PUNCTUATION + "]";
-const DocumentMentionsRegex = {
-  NAME,
-  PUNCTUATION
-};
-
-const CapitalizedNameMentionsRegex = new RegExp(
-  "(^|[^#])((?:" + DocumentMentionsRegex.NAME + "{" + 1 + ",})$)"
-);
-
-const PUNC = DocumentMentionsRegex.PUNCTUATION;
-
-const TRIGGERS = ["@"].join("");
-
-// Chars we expect to see in a mention (non-space, non-punctuation).
-const VALID_CHARS = "[^" + TRIGGERS + PUNC + "\\s]";
-
-// Non-standard series of chars. Each series must be preceded and followed by
-// a valid char.
-const VALID_JOINS =
-  "(?:" +
-  "\\.[ |$]|" + // E.g. "r. " in "Mr. Smith"
-  " |" + // E.g. " " in "Josh Duck"
-  "[" +
-  PUNC +
-  "]|" + // E.g. "-' in "Salier-Hellendag"
-  ")";
-
-const LENGTH_LIMIT = 75;
-
-const AtSignMentionsRegex = new RegExp(
-  "(^|\\s|\\()(" +
-  "[" +
-  TRIGGERS +
-  "]" +
-  "((?:" +
-  VALID_CHARS +
-  VALID_JOINS +
-  "){0," +
-  LENGTH_LIMIT +
-  "})" +
-  ")$"
-);
-
-// 50 is the longest alias length limit.
-const ALIAS_LENGTH_LIMIT = 50;
-
-// Regex used to match alias.
-const AtSignMentionsRegexAliasRegex = new RegExp(
-  "(^|\\s|\\()(" +
-  "[" +
-  TRIGGERS +
-  "]" +
-  "((?:" +
-  VALID_CHARS +
-  "){0," +
-  ALIAS_LENGTH_LIMIT +
-  "})" +
-  ")$"
-);
 
 function useMentionLookupService() {
   const [results, setResults] = useState<Array<UserInfo>>([]);
@@ -103,58 +39,6 @@ function useMentionLookupService() {
   return results;
 }
 
-function checkForCapitalizedNameMentions(
-  text: string,
-  minMatchLength: number
-): QueryMatch | null {
-  const match = CapitalizedNameMentionsRegex.exec(text);
-  if (match !== null) {
-    // The strategy ignores leading whitespace but we need to know it's
-    // length to add it to the leadOffset
-    const maybeLeadingWhitespace = match[1];
-
-    const matchingString = match[2];
-    if (matchingString != null && matchingString.length >= minMatchLength) {
-      return {
-        leadOffset: match.index + maybeLeadingWhitespace.length,
-        matchingString,
-        replaceableString: matchingString
-      };
-    }
-  }
-  return null;
-}
-
-function checkForAtSignMentions(
-  text: string,
-  minMatchLength: number
-): QueryMatch | null {
-  let match = AtSignMentionsRegex.exec(text);
-
-  if (match === null) {
-    match = AtSignMentionsRegexAliasRegex.exec(text);
-  }
-  if (match !== null) {
-    // The strategy ignores leading whitespace but we need to know it's
-    // length to add it to the leadOffset
-    const maybeLeadingWhitespace = match[1];
-
-    const matchingString = match[3];
-    if (matchingString.length >= minMatchLength) {
-      return {
-        leadOffset: match.index + maybeLeadingWhitespace.length,
-        matchingString,
-        replaceableString: match[2]
-      };
-    }
-  }
-  return null;
-}
-
-function getPossibleQueryMatch(text: string): QueryMatch | null {
-  const match = checkForAtSignMentions(text, 1);
-  return match === null ? checkForCapitalizedNameMentions(text, 3) : match;
-}
 
 class MentionTypeaheadOption extends TypeaheadOption {
   name: UserInfo;
@@ -190,7 +74,8 @@ function MentionsTypeaheadMenuItem({
       id={"typeahead-item-" + index}
       onMouseEnter={onMouseEnter}
       onClick={onClick}>
-      {option.name}
+      {option.picture}
+      {option.name.username}
     </li>
   );
 }
@@ -209,7 +94,7 @@ export default function NewMentionsPlugin(): JSX.Element | null {
       results
         .map(
           (result) =>
-            new MentionTypeaheadOption(result, <i className="icon user" />)
+            new MentionTypeaheadOption(result, <IconUser />)
         ),
     [results]
   );
@@ -238,7 +123,7 @@ export default function NewMentionsPlugin(): JSX.Element | null {
 
   const checkForMentionMatch = useCallback(
     (text: string) => {
-      const mentionMatch = getPossibleQueryMatch(text);
+      const mentionMatch = checkForMatch(text);
       return mentionMatch ? mentionMatch : null;
     },
     []
