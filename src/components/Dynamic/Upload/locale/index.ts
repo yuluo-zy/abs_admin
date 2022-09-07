8777fcb1435dbe8ac7cf76028d9b206f8a585605
yuluo <liyu@espressif.com>
2022-09-07 11:29:49 +0800

    add: 添加翻译内容

--- src/components/Dynamic/Upload/locale/index.ts
+++ src/components/Dynamic/Upload/locale/index.ts
@@ -1,5 +1,10 @@
 const i18n = {
-  "en-US": {},
+  "en-US": {
+    "message.ok": "Uploaded successfully",
+    "message.error": "Upload failed",
+    "message.size.error": "Upload failed, the upload file size exceeds the maximum value (20M)",
+    "message.type.error": "Upload failed, please upload the correct file type"
+  },
   "zh-CN": {
     "message.ok": "上传成功",
     "message.error": "上传失败",
