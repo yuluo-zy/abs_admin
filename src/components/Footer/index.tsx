bd214ee6d2f588138a11fdd173722dd1c420f4a9
yuluo <liyu@espressif.com>
2022-09-09 11:34:59 +0800

    add: 修改部分样式内容

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { Divider, Layout, Space } from "@arco-design/web-react";
+import { Layout } from "@arco-design/web-react";
 import cs from "classnames";
 import styles from "./style/index.module.less";
-import { Link } from "react-router-dom";
-import { TicketPath } from "@/utils/routingTable";
 import useLocale from "@/utils/useHook/useLocale";
 import locale from "./locale/index";
 
@@ -12,16 +10,16 @@ function Footer(props) {
   const t = useLocale(locale);
   return (
     <Layout.Footer className={cs(styles.footer, className)} {...restProps}>
-      <div style={{ marginTop: 5 }}>ESPRESSIF @2022 乐鑫信息科技（上海）股份有限公司</div>
-      {isLink && <div>
-        <Space>
-          Related Links:
-          <Divider type="vertical" />
-          <Link to={TicketPath}>{t["product.quality.qervice.work.order"]}</Link>
-          <Divider type="vertical" />
-          {/*<Link to={ProductQualityService}>{t['']}</Link>*/}
-        </Space>
-      </div>}
+      <div>{t["footer.info"]}</div>
+      {/*{isLink && <div>*/}
+      {/*  <Space>*/}
+      {/*    Related Links:*/}
+      {/*    <Divider type="vertical" />*/}
+      {/*    <Link to={TicketPath}>{t["product.quality.qervice.work.order"]}</Link>*/}
+      {/*    <Divider type="vertical" />*/}
+      {/*    /!*<Link to={ProductQualityService}>{t['']}</Link>*!/*/}
+      {/*  </Space>*/}
+      {/*</div>}*/}
 
 
     </Layout.Footer>
--- src/components/Footer/locale/index.ts
+++ src/components/Footer/locale/index.ts
@@ -1,9 +1,11 @@
 const i18n = {
   "en-US": {
-    "product.quality.qervice.work.order": "Product Quality Service Ticket"
+    "product.quality.qervice.work.order": "Product Quality Service Ticket",
+    "footer.info": "Copyright © 2022 Espressif Systems. All rights reserved."
   },
   "zh-CN": {
-    "product.quality.qervice.work.order": "产品品质工单系统"
+    "product.quality.qervice.work.order": "产品品质工单系统",
+    "footer.info": "Copyright © 2022 乐鑫信息科技（上海）股份有限公司。版权所有。"
   }
 };
 
