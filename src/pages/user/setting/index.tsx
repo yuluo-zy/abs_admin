9a84758ce8bd9827291dae2a106a04507dafcdd6
yuluo <liyu@espressif.com>
2022-09-09 15:04:08 +0800

    add: 修改密码添加

--- src/pages/user/setting/index.tsx
+++ src/pages/user/setting/index.tsx
@@ -4,15 +4,13 @@ import { Card, Tabs } from "@arco-design/web-react";
 import useLocale from "@/utils/useHook/useLocale";
 import locale from "./locale";
 import InfoHeader from "./header";
-import InfoForm from "./info";
 import Security from "./security";
-import Verified from "./verified";
 
 function UserInfo() {
   const t = useLocale(locale);
   const userInfo = useSelector((state: any) => state.userInfo);
   const loading = useSelector((state: any) => state.userLoading);
-  const [activeTab, setActiveTab] = useState("basic");
+  const [activeTab, setActiveTab] = useState("security");
   return (
     <div>
       <Card style={{ padding: "14px 20px" }}>
@@ -20,15 +18,15 @@ function UserInfo() {
       </Card>
       <Card style={{ marginTop: "16px" }}>
         <Tabs activeTab={activeTab} onChange={setActiveTab} type="rounded">
-          <Tabs.TabPane key="basic" title={t["userSetting.title.basicInfo"]}>
-            <InfoForm loading={loading} />
-          </Tabs.TabPane>
+          {/*<Tabs.TabPane key="basic" title={t["userSetting.title.basicInfo"]}>*/}
+          {/*  <InfoForm loading={loading} />*/}
+          {/*</Tabs.TabPane>*/}
           <Tabs.TabPane key="security" title={t["userSetting.title.security"]}>
             <Security />
           </Tabs.TabPane>
-          <Tabs.TabPane key="verified" title={t["userSetting.label.verified"]}>
-            <Verified />
-          </Tabs.TabPane>
+          {/*<Tabs.TabPane key="verified" title={t["userSetting.label.verified"]}>*/}
+          {/*  <Verified />*/}
+          {/*</Tabs.TabPane>*/}
         </Tabs>
       </Card>
     </div>
